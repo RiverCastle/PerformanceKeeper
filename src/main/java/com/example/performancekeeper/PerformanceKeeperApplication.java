f790a666e36afe933ee0ece3bd2103a7f3a11ba0
RiverCastle <rkdtjd12@gmail.com>
2023-11-27 10:57:34 +0900

    first commit

---
+++ src/main/java/com/example/performancekeeper/PerformanceKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.example.performancekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceKeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/performancekeeper/PerformanceKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.performancekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

58fa961f3eb42013a4cc2d750d014004ccc2d1b6
RiverCastle <rkdtjd12@gmail.com>
2023-11-28 16:10:53 +0900

    fix: 강의실 생성 기능 가입코드 암호화 추가

--- src/main/java/com/example/performancekeeper/api/course/CourseService.java
+++ src/main/java/com/example/performancekeeper/api/course/CourseService.java
@@ -4,17 +4,20 @@ import com.example.performancekeeper.api.member.MemberService;
 import com.example.performancekeeper.api.users.UserEntity;
 import com.example.performancekeeper.api.users.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @RequiredArgsConstructor
 public class CourseService {
+    private final BCryptPasswordEncoder passwordEncoder;
     private final CourseRepository courseRepository;
     private final MemberService memberService;
     private final UserService userService;
     public void createCourse(Long userId, CourseCreateDto courseCreateDto) {
         UserEntity userEntity = userService.checkUserEntity(userId);
+        courseCreateDto.setJoinCode(passwordEncoder.encode(courseCreateDto.getJoinCode()));
         CourseEntity courseEntity = CourseCreateDto.toEntity(courseCreateDto);
         courseEntity = courseRepository.save(courseEntity);
         memberService.createManager(userEntity, courseEntity);

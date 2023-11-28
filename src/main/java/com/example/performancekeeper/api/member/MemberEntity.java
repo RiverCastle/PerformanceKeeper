a42f357a9d2f0ba56b87c14c43c3ce8ccf2e2922
RiverCastle <rkdtjd12@gmail.com>
2023-11-28 22:47:11 +0900

    fix: 데이터 시간 기록 추가

--- src/main/java/com/example/performancekeeper/api/member/MemberEntity.java
+++ src/main/java/com/example/performancekeeper/api/member/MemberEntity.java
@@ -1,5 +1,6 @@
 package com.example.performancekeeper.api.member;
 
+import com.example.performancekeeper.api.common.BaseTimeEntity;
 import com.example.performancekeeper.api.course.CourseEntity;
 import com.example.performancekeeper.api.users.UserEntity;
 import jakarta.persistence.*;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
-public class MemberEntity {
+public class MemberEntity extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

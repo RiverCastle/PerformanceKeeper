83cdde69c1c8d2b52f177f3f006eb3137199f5fd
RiverCastle <rkdtjd12@gmail.com>
2023-11-28 21:04:36 +0900

    refactor: 미사용 메서드 삭제

--- src/main/java/com/example/performancekeeper/api/users/UserRepository.java
+++ src/main/java/com/example/performancekeeper/api/users/UserRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    Optional<UserEntity> findByUsername(String username);
-    Optional<UserEntity> findByUsernameAndAndEncodedPW(String username, String encodedPW);
+    Optional<UserEntity> findByUsernameAndDeletedAtIsNull(String username);
 }

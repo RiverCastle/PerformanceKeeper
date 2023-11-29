a3a239c8fdc27865e14983ab54ddfcf1c402e934
RiverCastle <rkdtjd12@gmail.com>
2023-11-29 10:51:31 +0900

    fix: 과제 시작일 추가

--- src/main/java/com/example/performancekeeper/api/task/TaskCreateDto.java
+++ src/main/java/com/example/performancekeeper/api/task/TaskCreateDto.java
@@ -2,15 +2,19 @@ package com.example.performancekeeper.api.task;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class TaskCreateDto {
     private String name;
     private String desc;
+    private LocalDate startAt;
 
     public static TaskEntity toEntity(TaskCreateDto dto) {
         TaskEntity entity = new TaskEntity();
         entity.setName(dto.getName());
         entity.setDesc(dto.getDesc());
+        entity.setStartAt(dto.getStartAt());
         return entity;
     }
 }

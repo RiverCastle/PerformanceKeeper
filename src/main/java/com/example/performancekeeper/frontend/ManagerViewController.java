f339ae1ec720453d2d649f1cfcbddd6daf203608
RiverCastle <rkdtjd12@gmail.com>
2023-11-30 17:35:00 +0900

    feat: 매니저 로그인 페이지 생성

---
+++ src/main/java/com/example/performancekeeper/frontend/ManagerViewController.java
@@ -0,0 +1,20 @@
+package com.example.performancekeeper.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/views/manager/")
+public class ManagerViewController {
+
+    @GetMapping("/login")
+    public String managerLoginPage() {
+        return "/manager/login-page";
+    }
+
+    @GetMapping("/main")
+    public String managerMainPage() {
+        return "/manager/index";
+    }
+}

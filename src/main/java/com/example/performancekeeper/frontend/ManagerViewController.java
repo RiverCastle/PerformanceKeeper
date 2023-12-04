735def4a36c1dae5613dded80c7dbf589c28988e
RiverCastle <rkdtjd12@gmail.com>
2023-12-04 17:03:08 +0900

    fix: / 제거

--- src/main/java/com/example/performancekeeper/frontend/ManagerViewController.java
+++ src/main/java/com/example/performancekeeper/frontend/ManagerViewController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/views/manager/")
+@RequestMapping("/views/manager")
 public class ManagerViewController {
 
     @GetMapping("/login")

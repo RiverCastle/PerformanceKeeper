a197b09989ab01055f54f375a91af29e347265e5
RiverCastle <rkdtjd12@gmail.com>
2023-12-06 09:51:22 +0900

    fix: 학생용 메인 페이지 로그아웃 스크립트 추가 및 강의실 정보 조회 스크립트 수정

--- src/main/resources/static/js/my-course-fetch.js
+++ src/main/resources/static/js/my-course-fetch.js
@@ -12,30 +12,32 @@ fetch('/api/course/myCourse', {
 
         } else {
             OverviewDtoList.forEach(course => {
-                console.log(course)
-                const course_id = course.id;
-                const course_table = document.createElement('table');
-
-                // course name
-                const course_name_cell = document.createElement('tr');
-                const course_link = document.createElement('a');
-                course_link.href = '/views/course/' + course_id;
-                course_link.textContent = course.name;
-                course_name_cell.appendChild(course_link);
-                course_table.appendChild(course_name_cell);
-
-                // role
-                const role_cell = document.createElement('tr');
-                role_cell.textContent = course.role;
-                course_table.appendChild(role_cell);
-
                 if (course.role === "Student") {
+                    const course_id = course.id;
+                    const course_table = document.createElement('table');
+
+                    // course name
+                    const course_name_cell = document.createElement('tr');
+                    const course_link = document.createElement('a');
+                    course_link.href = '/views/course/' + course_id;
+                    course_link.textContent = course.name;
+                    course_link.setAttribute('class', "d-inline-block text-truncate");
+                    course_name_cell.appendChild(course_link);
+                    course_table.appendChild(course_name_cell);
+
+                    // role
+                    const role_cell = document.createElement('tr');
+                    role_cell.textContent = course.role;
+                    course_table.appendChild(role_cell);
+
+
                     // progress
                     const progress_cell = document.createElement('tr');
-                    progress_cell.textContent = course.progress;
+                    progress_cell.textContent = course.progress + '%';
                     course_table.appendChild(progress_cell);
+
+                    wrapper.appendChild(course_table);
                 }
-                wrapper.appendChild(course_table);
             })
         }
     })

297cfa894426e38709272aef3d8806785d117e03
RiverCastle <rkdtjd12@gmail.com>
2023-12-18 09:43:42 +0900

    style: 코드 정렬 (줄바꿈)

--- src/main/java/com/example/performancekeeper/api/member/MemberService.java
+++ src/main/java/com/example/performancekeeper/api/member/MemberService.java
@@ -7,14 +7,23 @@ import java.util.List;
 
 public interface MemberService {
     void checkManagerMember(UserEntity user, CourseEntity course);
+
     List<MemberEntity> getAllStudentsOfThisCourse(CourseEntity course);
+
     MemberEntity checkStudentMember(UserEntity user, CourseEntity course);
+
     MemberEntity checkMember(UserEntity user, CourseEntity course);
+
     void createManagerMember(UserEntity user, CourseEntity course);
+
     MemberEntity createStudentMember(UserEntity user, CourseEntity course, JoinCourseDto joinCourseDto);
+
     List<MemberEntity> getMyMember(UserEntity user);
+
     void deleteStudentMember(MemberEntity member);
+
     void changeNickname(MemberEntity member, NicknameUpdateDto nicknameUpdateDto);
+
     MemberOverviewDto getMemberInfo(MemberEntity member);
 
     MemberEntity checkStudentMemberBeforeDelete(UserEntity user, CourseEntity course, LeaveRequestDto leaveRequestDto);

4e1fff274afe6b6c8a018ec23c90439e6358e55b
RiverCastle <rkdtjd12@gmail.com>
2023-12-20 13:57:24 +0900

    test: CourseRepositoryTest findByIdAndDeletedAtIsNull

--- src/test/java/com/example/performancekeeper/repository/CourseRepositoryTest.java
+++ src/test/java/com/example/performancekeeper/repository/CourseRepositoryTest.java
@@ -7,11 +7,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class CourseRepositoryTest {
     @Autowired
     CourseRepository courseRepository;
@@ -42,5 +45,38 @@ public class CourseRepositoryTest {
 
         // then
         Assertions.assertThat(courseRepository.findByIdAndDeletedAtIsNull(course.getId()).get()).isEqualTo(course);
+    }
+
+    @Test
+    void findByIdAndDeletedAtIsNull() {
+        // given
+        CourseEntity course1 = new CourseEntity();
+        CourseEntity course2 = new CourseEntity();
+        CourseEntity course3 = new CourseEntity();
+        course1.setName("test course name 1");
+        course1.setDescription("test course desc 1");
+        course1.setJoinCode("test course join code 1");
+        course2.setName("test course name 2");
+        course2.setDescription("test course desc 2");
+        course2.setJoinCode("test course join code 2");
+        course3.setId(4L);
+        course3.setDeletedAt(LocalDateTime.now());
+        course3.setName("test course name 3");
+        course3.setDescription("test course desc 3");
+        course3.setJoinCode("test course join code 3");
+
+        courseRepository.save(course1);
+        courseRepository.save(course2);
+
+
+        // when
+        CourseEntity courseEntity1 = courseRepository.findByIdAndDeletedAtIsNull(course1.getId()).get();
+        CourseEntity courseEntity2 = courseRepository.findByIdAndDeletedAtIsNull(course2.getId()).get();
+        Optional<CourseEntity> optionalCourseEntity = courseRepository.findByIdAndDeletedAtIsNull(course3.getId());
+
+        // then
+        Assertions.assertThat(course1).isEqualTo(courseEntity1);
+        Assertions.assertThat(course1).isNotEqualTo(courseEntity2);
+        Assertions.assertThat(optionalCourseEntity).isEmpty();
     }
 }

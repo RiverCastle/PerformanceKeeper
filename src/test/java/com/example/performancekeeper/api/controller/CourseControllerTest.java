6d892412f21ed6fa1e8bbbd6383a610055d4d17b
RiverCastle <rkdtjd12@gmail.com>
2024-02-27 12:16:58 +0900

    test: CourseControllerTest edit

--- src/test/java/com/example/performancekeeper/api/controller/CourseControllerTest.java
+++ src/test/java/com/example/performancekeeper/api/controller/CourseControllerTest.java
@@ -1,9 +1,12 @@
 package com.example.performancekeeper.api.controller;
 
 import com.example.performancekeeper.api.dto.course.CourseCreateDto;
+import com.example.performancekeeper.api.dto.course.CourseDescriptionUpdateDto;
+import com.example.performancekeeper.api.dto.course.CourseNameUpdateDto;
 import com.example.performancekeeper.api.facade.CourseControllerFacade;
 import com.example.performancekeeper.api.service.CourseService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,6 +36,7 @@ class CourseControllerTest {
         testAuthentication.setAuthenticated(true);
     }
     @Test
+    @DisplayName("강의실 생성 요청")
     void createCourse() {
         CourseCreateDto dto = new CourseCreateDto();
         dto.setName("test course");
@@ -42,22 +46,42 @@ class CourseControllerTest {
     }
 
     @Test
+    @DisplayName("강의실 목록 조회 요청")
     void getCourseList() {
+        String keyword = "";
+        courseController.getCourseList(keyword);
     }
 
     @Test
+    @DisplayName("나의 강의실 목록 조회 요청")
     void getMyCourses() {
+        courseController.getMyCourses(testAuthentication);
     }
 
     @Test
+    @DisplayName("강의실 세부 정보 조회 요청")
     void getCourseDetails() {
+        Long testCourseId = 1L;
+        courseController.getCourseDetails(testAuthentication, testCourseId);
     }
 
     @Test
+    @DisplayName("강의실 명 수정 요청")
     void updateCourseName() {
+        Long testCourseId = 1L;
+        CourseNameUpdateDto dto = new CourseNameUpdateDto();
+        dto.setNewName("new course name");
+
+        courseController.updateCourseName(testAuthentication, testCourseId, dto);
     }
 
     @Test
+    @DisplayName("강의실 소개 수정 요청")
     void updateDescriptionName() {
+        Long testCourseId = 1L;
+        CourseDescriptionUpdateDto dto = new CourseDescriptionUpdateDto();
+        dto.setNewDescription("new course desc");
+
+        courseController.updateDescriptionName(testAuthentication, testCourseId, dto);
     }
 }

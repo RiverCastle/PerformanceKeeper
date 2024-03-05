5fb93da04089c58f4f1d8f3a2b455d47ef4b4de2
RiverCastle <rkdtjd12@gmail.com>
2024-03-05 12:09:00 +0900

    docs: UserCreateDto JavaDoc add

--- src/main/java/com/example/performancekeeper/api/dto/user/UserCreateDto.java
+++ src/main/java/com/example/performancekeeper/api/dto/user/UserCreateDto.java
@@ -3,6 +3,9 @@ package com.example.performancekeeper.api.dto.user;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
+/**
+ * 회원 가입 요청 DTO 클래스
+ */
 @Getter
 public class UserCreateDto {
     @NotBlank
@@ -12,6 +15,13 @@ public class UserCreateDto {
     @NotBlank
     private String passwordCheck;
 
+    /**
+     * UserCreateDto 생성자
+     *
+     * @param username 아이디
+     * @param password 비밀번호
+     * @param passwordCheck 비밀번호 확인
+     */
     public UserCreateDto(String username, String password, String passwordCheck) {
         this.username = username;
         this.password = password;

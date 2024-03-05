0a72ec8d2f5b59e0c50b334d77e0766c3753422d
RiverCastle <rkdtjd12@gmail.com>
2024-03-05 14:09:38 +0900

    docs: MemberController JavaDoc add

--- src/main/java/com/example/performancekeeper/api/controller/MemberController.java
+++ src/main/java/com/example/performancekeeper/api/controller/MemberController.java
@@ -9,11 +9,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 강의실에 입실한 멤버 관련 Controller
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/course/{courseId}/member")
 public class MemberController {
     private final MemberControllerFacade performanceKeeperFacade;
+
+    /**
+     * 학생 멤버 등록 요청을 처리하는 메서드
+     *
+     * @param authentication
+     * @param courseId
+     * @param joinCourseDto 강의실 입실 코드를 담은 Dto 객체
+     */
     @PostMapping// 강의실 입실
     public void createStudentMember(Authentication authentication,
                                     @PathVariable("courseId") Long courseId,
@@ -22,6 +33,13 @@ public class MemberController {
         performanceKeeperFacade.createStudentMember(userId, courseId, joinCourseDto);
     }
 
+    /**
+     * 학생 멤버 삭제 요청을 처리하는 메서드
+     *
+     * @param authentication
+     * @param courseId
+     * @param leaveRequestDto 삭제 의사를 담은 Dto 객체
+     */
     @DeleteMapping// 강의실 퇴실
     public void leaveCourse(Authentication authentication,
                             @PathVariable("courseId") Long courseId,
@@ -30,6 +48,13 @@ public class MemberController {
         performanceKeeperFacade.deleteStudentMember(userId, courseId, leaveRequestDto);
     }
 
+    /**
+     * 멤버의 별명 수정 요청을 처리하는 메서드
+     *
+     * @param authentication
+     * @param courseId
+     * @param nicknameUpdateDto 새로운 별명을 담은 Dto 객체
+     */
     @PutMapping
     public void changeNickname(Authentication authentication,
                                @PathVariable("courseId") Long courseId,
@@ -38,6 +63,13 @@ public class MemberController {
         performanceKeeperFacade.changeNickname(userId, courseId, nicknameUpdateDto);
     }
 
+    /**
+     * 멤버 정보 조회 요청을 처리하는 메서드
+     *
+     * @param authentication
+     * @param courseId
+     * @return 멤버의 대략적인 정보를 담은 Dto 객체 반환
+     */
     @GetMapping
     public MemberOverviewDto getMemberInfo(Authentication authentication,
                                            @PathVariable("courseId") Long courseId) {
--- src/main/java/com/example/performancekeeper/api/dto/member/JoinCourseDto.java
+++ src/main/java/com/example/performancekeeper/api/dto/member/JoinCourseDto.java
@@ -2,6 +2,9 @@ package com.example.performancekeeper.api.dto.member;
 
 import lombok.Data;
 
+/**
+ * 유저가 강의실에 입실할 때 필요한 참여코드가 담기는 Dto 클래스
+ */
 @Data
 public class JoinCourseDto {
     private String joinCode;

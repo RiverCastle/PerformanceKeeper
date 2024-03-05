eccca26d23c4872f28d74b55c897b76a8ff54ef3
RiverCastle <rkdtjd12@gmail.com>
2024-03-05 12:46:28 +0900

    docs: CourseCreateDto JavaDoc add

--- src/main/java/com/example/performancekeeper/api/dto/course/CourseCreateDto.java
+++ src/main/java/com/example/performancekeeper/api/dto/course/CourseCreateDto.java
@@ -3,12 +3,15 @@ package com.example.performancekeeper.api.dto.course;
 import com.example.performancekeeper.api.entity.CourseEntity;
 import lombok.Data;
 
+/**
+ * 강의실 생성 요청 dto 클래스
+ * 강의실 이름, 참여코드, 소개 내용을 포함
+ */
 @Data
 public class CourseCreateDto {
     private String name;
     private String joinCode;
     private String description;
-
     public static CourseEntity toEntity(CourseCreateDto dto) {
         CourseEntity entity = new CourseEntity();
         entity.setName(dto.getName());

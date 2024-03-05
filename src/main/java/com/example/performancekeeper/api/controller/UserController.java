d7ee03894ac1cf7a06f2c1fc367d287bd0dba626
RiverCastle <rkdtjd12@gmail.com>
2024-03-05 12:08:51 +0900

    docs: UserController JavaDoc add

--- src/main/java/com/example/performancekeeper/api/controller/UserController.java
+++ src/main/java/com/example/performancekeeper/api/controller/UserController.java
@@ -10,12 +10,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 회원 관련 Controller입니다.
+ * 회원 가입 요청을 처리합니다.
+ *
+ */
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원가입 요청 처리 메서드입니다.
+     *
+     * @param userCreateDto
+     */
     @PostMapping
     public void signUp(@RequestBody @Valid UserCreateDto userCreateDto) {
         userService.createUser(userCreateDto);

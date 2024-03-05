18521cb985a6a17453a1e5c4883b647766f65dbb
RiverCastle <rkdtjd12@gmail.com>
2024-03-05 12:09:09 +0900

    docs: UserService JavaDoc add

--- src/main/java/com/example/performancekeeper/api/service/UserService.java
+++ src/main/java/com/example/performancekeeper/api/service/UserService.java
@@ -4,6 +4,18 @@ import com.example.performancekeeper.api.dto.user.UserCreateDto;
 import com.example.performancekeeper.api.entity.UserEntity;
 
 public interface UserService {
+    /**
+     * UserId로 유저의 존재를 확인하는 메서드입니다.
+     *
+     * @param userId
+     * @return 정상 수행 시 UserEntity 객체를 반환합니다.
+     */
     UserEntity checkUser(Long userId);
+
+    /**
+     * 회원을 등록하는 메서드입니다.
+     *
+     * @param userCreateDto
+     */
     void createUser(UserCreateDto userCreateDto);
 }

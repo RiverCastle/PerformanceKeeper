0a4da67f8a9129d162f1a29eaf2754d8888b6c6c
RiverCastle <rkdtjd12@gmail.com>
2024-04-25 11:22:15 +0900

    docs: Query counter 소개 추가

--- src/main/java/com/example/performancekeeper/api/common/queryCounter/ApiQueryCounter.java
+++ src/main/java/com/example/performancekeeper/api/common/queryCounter/ApiQueryCounter.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
+/**
+ * N + 1 문제를 탐지하는 목적으로 API 별로 쿼리의 개수를 카운트하는 역할의 클래스입니다.
+ */
 @Component
 @RequestScope
 @Getter
